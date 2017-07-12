6ceea83147e169dfe5f8f6e7f27c3c0879c5451b
dsturm <DaASturm@gmail.com>
2017-07-12 10:53:11 -0700

    maildir is a variable now

--- etc/cron.weekly/backup.sh
+++ etc/cron.weekly/backup.sh
@@ -2,5 +2,6 @@
 DATE = `date -u +%Y-%m-%dT%H:%MZ`
 ALLFILES = "/opt/bin/ /var/www/html/ /etc/apache2/ /etc/tacacs+/tac_plus.conf /etc/pam.d/tac_plus /etc/postfix/main.cf /etc/ssh/sshd_config /home/ /etc/cron.weekly/backup.sh"
 MAILER="MAILPLACEHOLDER"
+MAILDIR="/opt/bin"
 tar -zcvf /opt/tacbackups/"$DATE"tacbackups.tar.gz $ALLFILES
-/opt/bin/mailbackup | mail -aFrom:$MAILER -s "Tacacs+ Backup" -A /opt/tacbackups/"$DATE"tacbackups.tar.gz $MAILER
+$MAILDIR/mailbackup | mail -aFrom:$MAILER -s "Tacacs+ Backup" -A /opt/tacbackups/"$DATE"tacbackups.tar.gz $MAILER
