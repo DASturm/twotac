ccd87ef314c237688e53f5f7347a8f287e8ac9f8
dsturm <DaASturm@gmail.com>
2017-07-14 20:44:53 -0700

    Patch 0.9
    Mail has it's own folder /opt/tacmail/
    tac.conf relocated and renamed to /etc/twotac/twotac.conf
    All /opt/bin/ scripts have been stripped of their .sh suffix for more graceful global integration
    backup.sh files rewritten for new structure
    tacmanager has been rewritten with more logging and consistent naming with all the other rewrites
    tacuser and tacdelete now use the correct paths for things
    
    TODO: style.css seems to have integration problems
    test this newest version with a clean install on enterprise hardware, likely monday/next week.

--- opt/bin/reset.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm  /opt/bin/*
-rm -r /var/www/html
-rm -r /home/minibox/projects/Twotac-Files
-git clone https://github.com/dsturm-l/Twotac-Files.git /home/minibox/projects/Twotac-Files
-cp -r /home/minibox/projects/Twotac-Files/etc/ /
-cp -r /home/minibox/projects/Twotac-Files/opt/ /
-cp -r /home/minibox/projects/Twotac-Files/var/ /
-chmod u+x /opt/bin/*.sh
