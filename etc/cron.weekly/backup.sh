0f26e11f878d10d97924378edbf26b80f43241a7
dsturm <DaASturm@gmail.com>
2018-04-09 12:12:47 -0700

    Updated Variables
    Variables should show up in their new, proper places where appropriate

--- etc/cron.weekly/backup.sh
+++ etc/cron.weekly/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+TACCONF="/etc/twotac/twotac.conf"
 DATE=`date -u +%Y-%m-%dT%H:%MZ`
 ALLFILES="/etc/twotac/twotac.conf /etc/apache2/ /etc/tacacs+/tac_plus.conf /etc/pam.d/tac_plus /etc/postfix/main.cf /etc/ssh/sshd_config /etc/cron.weekly/ /opt/bin/ /opt/tacbackups/ /opt/tacconfigs/ /opt/tacmail/ /var/www/html/ /var/log/taclogs/ /home/"
-MAILER="MAILPLACEHOLDER"
-MAILDIR="/opt/tacmail"
+source $TACCONF
 tar -zcvf /opt/tacbackups/"$DATE"tacbackups.tar.gz $ALLFILES
 $MAILDIR/mailbackup | mail -aFrom:$MAILER -s "Tacacs+ Backup" -A /opt/tacbackups/"$DATE"tacbackups.tar.gz $MAILER
