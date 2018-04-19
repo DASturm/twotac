4c57fd7ba07c04b28eb53e2b0a4ae01abb4c7f72
dsturm <DaASturm@gmail.com>
2018-04-19 13:11:27 -0700

    Backup update #2

--- etc/cron.weekly/backup.sh
+++ etc/cron.weekly/backup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+TACLOG="/var/log/taclogs/taclog"
 TACCONF="/etc/twotac/twotac.conf"
 DATE=`date -u +%Y-%m-%dT%H:%MZ`
 ALLFILES="/etc/twotac/twotac.conf /etc/apache2/ /etc/tacacs+/tac_plus.conf /etc/pam.d/tac_plus /etc/postfix/main.cf /etc/ssh/sshd_config /etc/cron.weekly/ /opt/bin/ /opt/tacbackups/ /opt/tacconfigs/ /opt/tacmail/ /var/www/html/ /var/log/taclogs/ /home/"
 source $TACCONF
-if ! [[ $BACKUPDIR ]]; then
-	mkdir "$BACKUPDIR"
+if [[ $DOBACKUPS ]]; then
+	if ! [[ $BACKUPDIR ]]; then
+		mkdir "$BACKUPDIR"
+	fi
+	tar -zcvf "$BACKUPDIR""$DATE"tacbackups.tar.gz $ALLFILES
+	$MAILDIR/mailbackup | mail -aFrom:$MAILER -s "Tacacs+ Backup" -A "$BACKUPDIR""$DATE"tacbackups.tar.gz $MAILER
+else
+	echo "Backups disabled by twotac.conf" >> $TACLOG
 fi
-tar -zcvf "$BACKUPDIR""$DATE"tacbackups.tar.gz $ALLFILES
-$MAILDIR/mailbackup | mail -aFrom:$MAILER -s "Tacacs+ Backup" -A "$BACKUPDIR""$DATE"tacbackups.tar.gz $MAILER
