54b15eac8c38253e1823eabf7a7fff56ee5ead08
dsturm <DaASturm@gmail.com>
2018-04-19 13:07:24 -0700

    Backups update

--- etc/cron.weekly/backup.sh
+++ etc/cron.weekly/backup.sh
@@ -3,5 +3,8 @@ TACCONF="/etc/twotac/twotac.conf"
 DATE=`date -u +%Y-%m-%dT%H:%MZ`
 ALLFILES="/etc/twotac/twotac.conf /etc/apache2/ /etc/tacacs+/tac_plus.conf /etc/pam.d/tac_plus /etc/postfix/main.cf /etc/ssh/sshd_config /etc/cron.weekly/ /opt/bin/ /opt/tacbackups/ /opt/tacconfigs/ /opt/tacmail/ /var/www/html/ /var/log/taclogs/ /home/"
 source $TACCONF
-tar -zcvf /opt/tacbackups/"$DATE"tacbackups.tar.gz $ALLFILES
-$MAILDIR/mailbackup | mail -aFrom:$MAILER -s "Tacacs+ Backup" -A /opt/tacbackups/"$DATE"tacbackups.tar.gz $MAILER
+if ! [[ $BACKUPDIR ]]; then
+	mkdir "$BACKUPDIR"
+fi
+tar -zcvf "$BACKUPDIR""$DATE"tacbackups.tar.gz $ALLFILES
+$MAILDIR/mailbackup | mail -aFrom:$MAILER -s "Tacacs+ Backup" -A "$BACKUPDIR""$DATE"tacbackups.tar.gz $MAILER
