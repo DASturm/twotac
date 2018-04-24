da5cc35072a170883752b2921b4efb0848f756cf
dsturm <DaASturm@gmail.com>
2018-04-24 12:03:14 -0700

    Backups busted

--- etc/cron.weekly/backup.sh
+++ etc/cron.weekly/backup.sh
@@ -5,7 +5,9 @@ DATE=`date -u +%Y-%m-%dT%H:%MZ`
 ALLFILES="/etc/twotac/twotac.conf /etc/apache2/ /etc/tacacs+/tac_plus.conf /etc/pam.d/tac_plus /etc/postfix/main.cf /etc/ssh/sshd_config /etc/cron.weekly/ /opt/bin/ /opt/tacbackups/ /opt/tacconfigs/ /opt/tacmail/ /var/www/html/ /var/log/taclogs/ /home/"
 source $TACCONF
 if [[ $DOBACKUPS ]]; then
+	echo "DOBACKUPS=$DOBACKUPS"
 	if ! [[ $BACKUPDIR ]]; then
+		echo "BACKUPDIR=$BACKUPDIR"
 		mkdir "$BACKUPDIR"
 	fi
 	tar -zcvf "$BACKUPDIR""$DATE"tacbackups.tar.gz $ALLFILES
