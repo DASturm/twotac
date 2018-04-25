165def381e19e487a7b0c2e8a0ee71bffa4b124a
dsturm <DaASturm@gmail.com>
2018-04-25 13:26:26 -0700

    More shuffling

--- opt/tacbackups/backup.sh
+++ opt/tacbackups/backup.sh
@@ -4,12 +4,9 @@ TACCONF="/etc/twotac/twotac.conf"
 DATE=`date -u +%Y-%m-%dT%H:%MZ`
 ALLFILES="/etc/twotac/twotac.conf /etc/apache2/ /etc/tacacs+/tac_plus.conf /etc/pam.d/tac_plus /etc/postfix/main.cf /etc/ssh/sshd_config /etc/cron.weekly/ /opt/bin/ /opt/tacbackups/ /opt/tacconfigs/ /opt/tacmail/ /var/www/html/ /var/log/taclogs/ /home/"
 source $TACCONF
+echo "Backup initiated" >> $TACLOG
 if [[ $DOBACKUPS ]]; then
-	echo "DOBACKUPS=$DOBACKUPS"
-	if ! [[ $BACKUPDIR ]]; then
-		echo "BACKUPDIR=$BACKUPDIR"
-		sudo mkdir "$BACKUPDIR"
-	fi
+	echo "Running Backup" >> $TACLOG
 	tar -zcvf "$BACKUPDIR""$DATE"tacbackups.tar.gz $ALLFILES
 	$MAILDIR/mailbackup | mail -aFrom:$MAILER -s "Tacacs+ Backup" -A "$BACKUPDIR""$DATE"tacbackups.tar.gz $MAILER
 else
