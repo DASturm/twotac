495e3472b119e2d1cd2a8d2223a050de324632de
dsturm <DaASturm@gmail.com>
2018-04-30 12:15:37 -0700

    More variable/directory changes for compatibilty with updates

--- opt/tacbackups/backup.sh
+++ opt/tacbackups/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-TACLOG="/var/log/taclogs/taclog"
+TACLOG="/var/log/twotac/taclog"
 TACCONF="/etc/twotac/twotac.conf"
 DATE=`date -u +%Y-%m-%dT%H:%MZ`
 ALLFILES="/etc/twotac/twotac.conf /etc/apache2/ /etc/tacacs+/tac_plus.conf /etc/pam.d/tac_plus /etc/postfix/main.cf /etc/ssh/sshd_config /etc/cron.weekly/ /opt/bin/ /opt/tacbackups/ /opt/tacconfigs/ /opt/tacmail/ /var/www/html/ /var/log/taclogs/ /home/"
--- opt/tacbackups/twotaclogrotate.sh
+++ opt/tacbackups/twotaclogrotate.sh
@@ -1,4 +1,4 @@
 $DATE=$(date +"%m-%d-%y")
-cp /var/log/taclogs/taclog /var/log/taclogs/taclog$DATE
-rm /var/log/taclogs/taclog
-echo -e "END WEEKLY LOG\n$DATE" >> /var/log/taclogs/taclog$DATE
+cp /var/log/twotac/taclog /var/log/twotac/taclog$DATE
+rm /var/log/twotac/taclog
+echo -e "END WEEKLY LOG\n$DATE" >> /var/log/twotac/taclog$DATE
